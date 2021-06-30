db4b5f6503c0a9e018b088e5517d8ff8f7cfb782
Ganesh <ganeshgavhad9@gmail.com>
2021-06-30 21:30:57 +0530

    Added: Function of Checking Equality Of Two Length

--- linecomparison/CompareLine.java
+++ linecomparison/CompareLine.java
@@ -13,31 +13,48 @@ package linecomparison;
 public class CompareLine {
 	
 /**
+ * @return Length of line 1
  * @coordinatesForLine1 is a method for line 1 coordinates And calculating length of line 2.
  */
-public void coordinatesForLine1() {
+public double coordinatesForLine1() {
 	double x1= 12d;
 	double y1= 10d;
 	double x2= 8d;
 	double y2= 5d;
 	double lengthOfLine1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 	System.out.println("Length of line 1 : "+lengthOfLine1);
+	return lengthOfLine1;
 
 }
 /**
+ * @return Length of line 2
  * @coordinatesForLine1 is a method for line 2 coordinates And calculating length of line 1.
  */
-public void coordinatesForLine2() {
+public double coordinatesForLine2() {
 	double x1= 11d;
 	double y1= 13d;
 	double x2= 7d;
 	double y2= 9d;
 	double lengthOfLine2 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 	System.out.println("Length of line 2 : "+lengthOfLine2);
+	return lengthOfLine2;
 }
-
 /**
- * Main Method Of  Class
+ * @equalityOfLength is a method for checking equality of two length using java equal method
+ */
+public void equalityOfLength() {
+	double line1 = coordinatesForLine1();
+	String s1 = Double.toString(line1);
+	double line2 = coordinatesForLine2();
+	String s2 = Double.toString(line2);
+	if (s1.equals(s2))
+		System.out.println("Both lines are same in length");
+	else
+		System.out.println("Both the lines are different in length");
+
+}
+/**
+ * @Main is a main method of  class
  */
 
 public static void main(String[] args) {
@@ -45,5 +62,6 @@ public static void main(String[] args) {
 	CompareLine length = new CompareLine();
 	length.coordinatesForLine1();	
 	length.coordinatesForLine2();
+	length.equalityOfLength();
 	}
 }
