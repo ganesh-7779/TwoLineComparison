02238dfcd6d27ebd513910776e7fa67300bab61f
Ganesh <ganeshgavhad9@gmail.com>
2021-06-30 21:41:20 +0530

    Added: Function of Camparing Two Length using ComapareTo method

--- linecomparison/CompareLine.java
+++ linecomparison/CompareLine.java
@@ -5,6 +5,7 @@
  * @version 1.0
  ***********************************************************
  */
+
 package linecomparison;
 
 /**
@@ -16,6 +17,7 @@ public class CompareLine {
  * @return Length of line 1
  * @coordinatesForLine1 is a method for line 1 coordinates And calculating length of line 2.
  */
+	
 public double coordinatesForLine1() {
 	double x1= 12d;
 	double y1= 10d;
@@ -26,10 +28,12 @@ public double coordinatesForLine1() {
 	return lengthOfLine1;
 
 }
+
 /**
  * @return Length of line 2
  * @coordinatesForLine1 is a method for line 2 coordinates And calculating length of line 1.
  */
+
 public double coordinatesForLine2() {
 	double x1= 11d;
 	double y1= 13d;
@@ -39,9 +43,11 @@ public double coordinatesForLine2() {
 	System.out.println("Length of line 2 : "+lengthOfLine2);
 	return lengthOfLine2;
 }
+
 /**
  * @equalityOfLength is a method for checking equality of two length using java equal method
  */
+
 public void equalityOfLength() {
 	double line1 = coordinatesForLine1();
 	String s1 = Double.toString(line1);
@@ -51,8 +57,26 @@ public void equalityOfLength() {
 		System.out.println("Both lines are same in length");
 	else
 		System.out.println("Both the lines are different in length");
-
 }
+
+/**
+ * @compareLength this is a method for comparing length of two line as string using compareTo method of java.
+ */
+
+public void compareLength() {
+	double line1 = coordinatesForLine1();
+	String s1 = Double.toString(line1);
+	double line2 = coordinatesForLine2();
+	String s2 = Double.toString(line2);
+	if (s1.compareTo(s2) < 0) {
+		System.out.println(s1 + " is less than the" + s2);
+		System.out.println("s2 string is greater");
+	} else {
+		System.out.println(s1 + " is greater than the " + s2);
+		System.out.println("s1 string is greater");
+	}
+}
+
 /**
  * @Main is a main method of  class
  */
@@ -63,5 +87,6 @@ public static void main(String[] args) {
 	length.coordinatesForLine1();	
 	length.coordinatesForLine2();
 	length.equalityOfLength();
+	length.compareLength();
 	}
 }
