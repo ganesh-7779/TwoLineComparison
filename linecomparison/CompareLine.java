e2e0eef643d5b84a326f74d55fa9bc715d24ea3c
Ganesh <ganeshgavhad9@gmail.com>
2021-06-30 20:32:02 +0530

    Welcome to Line Comparison Computation Program

---
+++ linecomparison/CompareLine.java
@@ -0,0 +1,21 @@
+
+/**
+ * *********************************************************
+ * Purpose: Ability to work with java Programming Construct.
+ * @author Ganesh Gavhad
+ * @version 1.0
+ ***********************************************************
+ */
+package linecomparison;
+/**
+ * CompareLine is main Class Of Program
+ */
+public class CompareLine {
+
+/**
+ * Main Method Of  Class
+ */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
